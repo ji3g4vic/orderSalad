57f94b3c4dc4e0b62017402add9c46c5bda56bc3
Vic <ji3g4vic03@gmail.com>
2023-02-07 11:06:29 +0800

    Initial Commit

---
+++ orderSalad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  orderSalad
+//
+//  Created by 王衍智 on 2023/2/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
